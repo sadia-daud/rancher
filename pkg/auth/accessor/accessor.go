145532284e66a8b48ea11ed87c8ee9d822410d4a
Andreas Kupries <andreas.kupries@suse.com>
2024-10-23 12:43:30 +0200

    feat: bring in the TokenAccessor interface to hide Norman/Imperative behind where Rancher will work with both in some capacity.

---
+++ pkg/auth/accessor/accessor.go
@@ -0,0 +1,24 @@
+package accessor
+
+import (
+	v3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// TokenAccessor is an interface hiding the details of the different Token
+// structures from the parts of Rancher having to work with both. The structures
+// currently implementing this interface are Norman in file
+// `pkg/apis/management.cattle.io/v3/authn_types.go`, and imperative in file
+// `pkg/apis/ext.cattle.io/v1/types.go`
+type TokenAccessor interface {
+	GetName() string
+	GetIsEnabled() bool
+	GetIsDerived() bool
+	GetAuthProvider() string
+	GetUserID() string
+	GetUserPrincipal() v3.Principal
+	GetProviderInfo() map[string]string
+	ObjClusterName() string
+	GetGroupPrincipals() []v3.Principal
+	GetLastUsedAt() *metav1.Time
+}
